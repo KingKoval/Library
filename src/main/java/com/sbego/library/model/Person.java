a97cf364e3364586840bc6b02bbcbfb09dbdcfa8
Bohdan Kovalchuk <b.kovalchuk14@gmail.com>
2023-05-26 16:15:09 +0200

    Changed import libraries

--- src/main/java/com/sbego/library/model/Person.java
+++ src/main/java/com/sbego/library/model/Person.java
@@ -1,9 +1,6 @@
 package com.sbego.library.model;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 public class Person {
     private int person_id;
